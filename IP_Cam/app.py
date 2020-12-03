1b676b4c7e4bb3d781a36afd001a100a460bda5b
Abduarahman Bazena <abduarahman.bazena@gmail.com>
2020-12-03 12:45:36 +0100

    added IP_Cam

---
+++ IP_Cam/app.py
@@ -0,0 +1,32 @@
+import yaml
+from flask import Flask, render_template, Response
+import cv2
+import pathlib
+
+app = Flask(__name__)
+
+camera = cv2.VideoCapture(0)
+
+def gen_frames():  
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    with open(str(pathlib.Path(__file__).parent.absolute()) + "/settings.yaml", 'r') as stream:
+        settings = yaml.safe_load(stream)
+    app.run(host=settings['HOST'], port=settings['PORT'] , debug=settings['DEBUG'])
